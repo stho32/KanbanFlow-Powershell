54c12c9498c48ce692eb029c042d29a28978f574
Stefan Hoffmann <Stefan.Hoffmann@mahr-edv.de>
2018-04-26 12:06:18 +0200

    New CmdLet to update a task name

--- Source/Kanbanflow.psm1
+++ Source/Kanbanflow.psm1
@@ -92,7 +92,37 @@ function Get-TasksFlat {
     return @( $result )
 }
 
+<#
+.Synopsis
+    Get all Tasks from a kanban board - but not nested within 
+    columns
+.DESCRIPTION
+    Gets all tasks for a kanbanflow board
+    
+.EXAMPLE
+    Example how to use this cmdlet
+.EXAMPLE
+    Another example
+#>
+function Update-TaskName {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$ApiToken,
+        [Parameter(Mandatory=$true)]
+        [string]$TaskId,
+        [Parameter(Mandatory=$true)]
+        [string]$NewTaskName
+    )
+
+    $credentials = "apiToken:" + $ApiToken
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($credentials))
+
+    Invoke-RestMethod -Headers @{Authorization=("Basic " + $base64AuthInfo)} -Method Post -Uri https://kanbanflow.com/api/v1/tasks/$TaskId -ContentType "application/json" -Body "{ `"name`":`"$NewTaskName`"}"
+}
+
 # Exports for the module
 Export-ModuleMember -Function Get-Board
 Export-ModuleMember -Function Get-Tasks
 Export-ModuleMember -Function Get-TasksFlat
+Export-ModuleMember -Function Update-TaskName
