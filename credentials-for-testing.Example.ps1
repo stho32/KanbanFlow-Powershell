d248bf468b69e7f2b5d59d8c61ea01667786fe0e
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-04-28 11:51:20 +0200

    Add example for test board api token

---
+++ credentials-for-testing.Example.ps1
@@ -0,0 +1,6 @@
+<#
+    This file contains a connection to a default Kanban board in 
+    kanbanflow that you want to use in your unit tests.
+#>
+
+$testBoardApiToken = "<YOUR TESTING BOARD API KEY HERE>"
