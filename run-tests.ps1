8448144c822486dd4fcc4942af78abd2e430b071
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-04-28 11:25:45 +0200

    Add scripts for running unit tests

---
+++ run-tests.ps1
@@ -0,0 +1,7 @@
+<#
+
+    Execute all unit tests
+
+#>
+
+Invoke-Pester -Script "./Source/Kanbanflow.Tests.ps1"
---
+++ run-tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#
+# run powershell tests from bash for powershell core on linux or mac
+#
+pwsh ./run-tests.ps1
