f184c27f40e2510816bd425ec07936f2eac96c18
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-05-01 11:52:44 +0200

    New-KanbanflowAuthHeader is tested now

--- Documentation/Kanbanflow.Tests.ps1
+++ Documentation/Kanbanflow.Tests.ps1
@@ -6,13 +6,6 @@
 Import-Module $PSScriptRoot/Kanbanflow.psm1
 . $PSScriptRoot/../credentials-for-testing.ps1
 
-Describe 'New-KanbanflowAuthHeader' {
-    It 'creates this known test auth header correctly' {
-        $authHeader = New-KanbanflowAuthHeader -ApiToken "abc"
-        $authHeader["Authorization"] | Should -BeExactly "Basic YXBpVG9rZW46YWJj"
-    }
-}
-
 Describe 'Get-KanbanflowBoard' {
     $board = Get-KanbanflowBoard -ApiToken $testBoardApiToken
 
