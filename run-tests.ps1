8533317cf64b7a6b76f1a95a4ad7daffd5258130
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-05-05 10:02:32 +0000

    Changed setup.ps1 again for google cloud

--- run-tests.ps1
+++ run-tests.ps1
@@ -3,6 +3,6 @@
     Execute all unit tests
 
 #>
-
+Import-Module Pester -RequiredVersion 4.3.1
 $filesToCover = Get-ChildItem .\Source -File -Recurse -Include *.ps1
 Invoke-Pester -Path "./Tests" -CodeCoverage $filesToCover
--- setup.ps1
+++ setup.ps1
@@ -5,15 +5,5 @@
 
 #>
 
-Write-Host "Checking for 'Pester' for unit test support..."
-Write-Host "To ensure we have the correct version on a cleanly installed VM on azure or google cloud we need to remove it..."
-Remove-Module Pester 
-Write-Host "Removing the module wont work on new VMs but on cleanly installed computers."
-Write-Host "Thus we can now assume that, if we find a directory there, it has not been cleanly installed..."
-if (Test-Path -Path "C:\Program Files\WindowsPowerShell\Modules\Pester" ) {
-    Remove-Item -Force -Recurse -Path "C:\Program Files\WindowsPowerShell\Modules\Pester"
-}
-if (Test-Path -Path "C:\Program Files (x86)\WindowsPowerShell\Modules\Pester" ) {
-    Remove-Item -Force -Recurse -Path "C:\Program Files (x86)\WindowsPowerShell\Modules\Pester"
-}
+Write-Host "Setting up for 'Pester' for unit test support..."
 Install-Module Pester -Force -SkipPublisherCheck
