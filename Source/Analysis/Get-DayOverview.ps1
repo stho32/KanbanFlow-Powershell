69325666bd0fe89f015eb599ed2db0444a2e54d3
Stefan Hoffmann <sthoffmann@onlinehome.de>
2018-05-09 21:26:25 +0200

    Changed Get-DayOverview so it groups tasks by color in the report

--- Source/Analysis/Get-DayOverview.ps1
+++ Source/Analysis/Get-DayOverview.ps1
@@ -18,33 +18,50 @@ function Get-DayOverview {
     $to = $from.AddDays(1).AddSeconds(-1)
     $entries = Get-TimeEntries -From $from -To $to -ApiToken $ApiToken
     $tasks = Get-Task -ApiToken $ApiToken -Flat
-    #Write-Output ($tasks | ConvertTo-Json)    
 
+    <# We enrich the time entries with task information that we need
+       for grouping later. #>
     foreach ($entry in $entries) {
-        $taskName = ($tasks | Where-Object _id -eq $entry.taskId).name
+        $task = ($tasks | Where-Object _id -eq $entry.taskId)
+        
+        $taskName = $task.name
+        $color = $task.color 
+        
         $fromDateTime = $entry.startTimeStamp
         $tillDateTime = $entry.endTimeStamp
         $durationInMinutes = [Math]::Round(($tillDateTime - $fromDateTime).TotalMinutes,0)
 
         $entry | Add-Member NoteProperty -Name taskName -Value $taskName
         $entry | Add-Member NoteProperty -Name durationInMinutes -Value $durationInMinutes
+        $entry | Add-Member NoteProperty -Name color -Value $color
     }
 
+    <# Then we group by user and color (task type), finally per task
+       to create a statistic about the time that went in every task type and
+       task in pomodoros. #>
     $entries | Group-Object userId | ForEach-Object {
-        Write-Output "User:" $_.name -ForegroundColor "White"
+        Write-Output "User: $($_.name)"
         $totalP = 0.0
-        $userTasks = $_.group | Group-Object taskName
-        $userTasks | ForEach-Object {
-            $totalMinutes = ($_.group | Measure-Object durationInMinutes -Sum).Sum
-            $pomodoros = [Math]::Round( $totalMinutes / 25, 1 )
-            $totalP += $pomodoros
-            Write-Output ("  - " + $_.name + " (" + $totalMinutes + " min, " + $pomodoros + " P)") -ForegroundColor "Green"
+
+        $userColors = $_.group | Group-Object color
+        $userColors | ForEach-Object {
+            Write-Output "    Color: $($_.name)"
+            $sectionsP = 0.0
+
+            $userTasks = $_.group | Group-Object taskName
+            $userTasks | ForEach-Object {
+                $totalMinutes = ($_.group | Measure-Object durationInMinutes -Sum).Sum
+                $pomodoros = [Math]::Round( $totalMinutes / 25, 1 )
+                $sectionsP += $pomodoros
+                $totalP += $pomodoros
+                Write-Output "      - $($_.name) ($pomodoros P)" 
+            }
+
+            Write-Output "    = $sectionsP P"
+            Write-Output ""
         }
 
-        Write-Output "=" $totalP "P" -ForegroundColor "White"
+        Write-Output " = $totalP P"
         Write-Output ""
-        #Write-Output ($_.group | Group-Object taskName | ConvertTo-Json) 
     }
-    
-
 }
