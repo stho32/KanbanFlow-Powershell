97ec32d49a134c56083a019c70b2b6cd24d3b2c2
Kevin Glier <Kevin.Glier@mahr-edv.de>
2018-06-01 17:19:05 +0200

    Add comments to Get-DayOverview

--- Source/Analysis/Get-DayOverview.ps1
+++ Source/Analysis/Get-DayOverview.ps1
@@ -30,6 +30,7 @@ function Get-DayOverview {
         $color = $task.color 
         $number = $task.number.value
         $status = $task.columnName
+        $id = $task._id
 
         $fromDateTime = $entry.startTimeStamp
         $tillDateTime = $entry.endTimeStamp
@@ -40,22 +41,26 @@ function Get-DayOverview {
         $entry | Add-Member NoteProperty -Name color -Value $color
         $entry | Add-Member NoteProperty -Name number -Value $number
         $entry | Add-Member NoteProperty -Name status -Value $status 
+        $entry | Add-Member NoteProperty -Name id -Value $id 
 
     }
 
 
+    $template_lineBreak = "\n"
     $template_StartUser = "User: #username#"
     $template_StartColor = "    Color: #color#"
-    $template_Task = "      - Nr. #nr#, #column# : #taskname# (#pomodoros# P)"
+    $template_Task = "      - Nr. #nr#, #column# : #taskname# (#pomodoros# P)" + $template_lineBreak + "#comments#"
     $template_EndColor = "    = #sectionsP# P"
     $template_EndUser = " = #totalP# P"
+    
 
     if ($format -eq "Html") {
-        $template_StartUser = "<strong>User: #username#</strong><br/>"
-        $template_StartColor = "<strong>Color: #color#</strong><br/><ul>"
-        $template_Task = "      <li><span style=`"color:#009dd3`">Nr. #nr#, #column# : <strong>#taskname#</strong> (#pomodoros# P)</span><br/>_<br/></li>"
-        $template_EndColor = "</ul>    = #sectionsP# P<br/>"
-        $template_EndUser = "<br/><br><strong> = #totalP# P</strong><br/><br/>"
+        $template_lineBreak = "<br/>"
+        $template_StartUser = "<strong>User: #username#</strong>" + $template_lineBreak
+        $template_StartColor = "<strong>Color: #color#</strong>" + $template_lineBreak + "<ul>"
+        $template_Task = "      <li><span style=`"color:#009dd3`">Nr. #nr#, #column# : <strong>#taskname#</strong> (#pomodoros# P)</span>" + $template_lineBreak + "#comments#</li>"
+        $template_EndColor = "</ul>    = #sectionsP# P" + $template_lineBreak
+        $template_EndUser = $template_lineBreak + $template_lineBreak +"<strong> = #totalP# P</strong>" + $template_lineBreak + $template_lineBreak
     }
 
     $result = [System.Text.StringBuilder]::new()
@@ -88,6 +93,18 @@ function Get-DayOverview {
                 $taskText = $taskText.Replace("#pomodoros#", $pomodoros.ToString())
                 $taskText = $taskText.Replace("#taskname#", $_.name)
                 $taskText = $taskText.Replace("#column#", $status)
+                
+                $comments = ""
+                foreach ($comment in (Get-Comments -ApiToken $ApiToken -TaskId $_.group[0].id)) {
+                    
+                    if ($comment.createdTimestamp -gt $from -and `
+                        $comment.createdTimestamp -lt $to -and `
+                        $comment.authorUserId -eq $_.group[0].userId) {
+                        $comments += $comment.text + $template_lineBreak + $template_lineBreak
+                    }
+                }
+                $comments = if ($comments -eq "") { ("_" + $template_lineBreak + $template_lineBreak) } else { $comments }
+                $taskText = $taskText.Replace("#comments#", $comments)
 
                 [void]$result.AppendLine($taskText)
             }
