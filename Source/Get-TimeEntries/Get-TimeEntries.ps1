bd0d114cb96b4758c62caa236f49875c73a10ac7
Stefan Hoffmann <stho32github@gmail.com>
2019-02-24 11:18:57 +0100

    Update to a more modern approach

--- Source/Get-TimeEntries/Get-TimeEntries.ps1
+++ Source/Get-TimeEntries/Get-TimeEntries.ps1
@@ -13,15 +13,16 @@ function Get-TimeEntries {
         [Parameter(Mandatory=$true)]
         [DateTime]$To
     )
-    
-    $fromAsString = (ConvertTo-KanbanflowDateTime $from)
-    $toAsString = (ConvertTo-KanbanflowDateTime $to)
 
-    $parameter = New-Object PSObject
-    $parameter | Add-Member NoteProperty -Name from -Value $fromAsString
-    $parameter | Add-Member NoteProperty -Name to -Value $toAsString
-    $parameter | Add-Member NoteProperty -Name limit -Value 200
-    
-    Invoke-KanbanflowApi -ApiToken $ApiToken -Method "Get" -Command "time-entries" -Parameters $parameter
+    Process {
 
+        $parameter = New-Object PSObject -Property @{
+            from = (ConvertTo-KanbanflowDateTime $from)
+            to = (ConvertTo-KanbanflowDateTime $to)
+            limit = 200
+        }
+        
+        Invoke-KanbanflowApi -ApiToken $ApiToken -Method "Get" -Command "time-entries" -Parameters $parameter
+
+    }
 }
